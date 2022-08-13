7497f94962e4a2f6e6d636dcaf63c492e0d8242c
Your Name <mihirsaini25@gmail.com>
2022-08-13 23:45:31 +0530

    db added

--- app.js
+++ app.js
@@ -11,35 +11,132 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+const itemsSchema={
+  name:String
+} 
+const Item=mongoose.model( //using the singular name for the items collection
+   "Item",itemsSchema
+);
+const item1=new Item({
+  name:"Welcome"
+});
+const item2=new Item({
+  name:"Click + to add"
+});
+const item3=new Item({
+  name:"<-- Click to delete"
+});
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+const defaultItems=[item1,item2,item3];
+ 
+const listSchema={
+  name:String,
+  items:[itemsSchema] //an array of item schema documents
+}
+
+const List=mongoose.model("List",listSchema);
+// Item.insertMany(defaultItems, function(err){  //to insert many items at once into the items collection
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Success!");
+//   }
+// });
 const workItems = [];
 
 app.get("/", function(req, res) {
-
+Item.find(function(err, items){
+  if(err){
+    console.log(err);
+  }
+  else{
+    if(items.length===0){
+      Item.insertMany(defaultItems, function(err){  //to insert many items at once into the items collection
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Success!");
+  }
+});
+    }
+    else{
+    res.render("list", {listTitle: day, newListItems: items});
+  }}
+});
 const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  
+  
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  const itemName  = req.body.newItem;
+  const item=new Item({
+    name:itemName
+  });
+  const listName=req.body.list;
+  if(listName===day){
+    item.save();
     res.redirect("/");
   }
+  else{
+    List.findOne({name:listName},function(err,lists){ //to find the custom list 
+      lists.items.push(item); //pushing the new item in the items array of the custom list
+      lists.save();
+      res.redirect("/"+listName); 
+    });
+  }
+  item.save();
+  res.redirect("/");
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+app.post("/delete",function(req,res){
+ const checkedItemId=req.body.checkbox; //returns the value of the checkbox input
+  Item.findByIdAndRemove(checkedItemId,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("success!");
+      res.redirect("/");
+    }
+  });
+ });
+
+ app.get("/:listName",function(req,res){
+   const listName=req.params.listName;
+   List.findOne({name:listName},function(err, lists){
+     if(!err){
+       if(!lists){
+         const list=new List({
+           name:listName,
+           items:defaultItems
+         });
+         list.save();
+         res.redirect("/"+listName);
+       }
+       else{
+         res.render("list", {listTitle: lists.name, newListItems: lists.items});
+       }
+     }
+   });
+  
+ });
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
 
 app.get("/about", function(req, res){
   res.render("about");
