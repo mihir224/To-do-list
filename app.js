9fe5049a33c005a0408615232235d10be8d9dfc7
Your Name <mihirsaini25@gmail.com>
2022-08-14 16:53:48 +0530

    src added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-mihir:mugeer%40224@cluster0.ly91ewg.mongodb.net/todolistDB", {useNewUrlParser: true});
 const itemsSchema={
   name:String
 } 
