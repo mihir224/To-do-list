ecef6f72d35289f59c05faa13130ceef8c68c452
Your Name <mihirsaini25@gmail.com>
2022-08-19 23:57:28 +0530

    app.js modified

--- app.js
+++ app.js
@@ -148,5 +148,8 @@ const day = date.getDate();
 app.get("/about", function(req, res){
   res.render("about");
 });
-
-app.listen(process.env.PORT);
+let port=process.env.PORT
+if(port==null||port==""){
+  port=3000;
+}
+app.listen(port);
